0ba59d9ff4fd1c8108b7e117e0e2a9ea6a488b5b
sanchitadevi <thesanchitadevi@gmail.com>
2022-10-25 23:42:53 +0600

    register page added

--- src/Components/Shared/Login/Login.js
+++ src/Components/Shared/Login/Login.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login</h1>
-        </div>
+        <div></div>
     );
 };
 
