51e322d8e5451a9b51eafd57f2df2d6a894dd1aa
sanchitadevi <thesanchitadevi@gmail.com>
2022-10-26 12:36:52 +0600

    loader added

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -8,6 +8,13 @@ const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
+    if (loading) {
+        return (<div className="flex items-center justify-center mt-10" >
+            <div className="w-16 h-16 border-b-2 border-slate-600 rounded-full animate-spin"></div>
+        </div >
+        )
+    }
+    
     if (!user) {
         return <Navigate to='/login' state={{ from: location }} replace></Navigate>
     }
