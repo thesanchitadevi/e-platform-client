4ddd343757df70f12caafb3fda1a39675dbccf3c
sanchitadevi <thesanchitadevi@gmail.com>
2022-10-27 18:24:45 +0600

    changes done

--- src/Components/Shared/CourseCard/CourseCard.js
+++ src/Components/Shared/CourseCard/CourseCard.js
@@ -1,43 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactToPdf from "react-to-pdf";
 
+
+const ref = React.createRef();
 
 const CourseCard = ({ course, exampleRef }) => {
     // console.log(course);
 
-    const {_id, details, image_url, rating, title } = course;
+    const { _id, details, image_url, rating, title } = course;
 
     return (
         <div>
             <div className="max-w-xs overflow-hidden bg-slate-200 rounded-lg shadow-lg dark:bg-gray-800">
-                <div className="px-4 py-2">
+                <div ref={ref} className="px-4 py-2">
                     <h1 className="text-xl font-bold text-slate-800 uppercase dark:text-white" >{title}</h1>
-                    <div  className="mt-1 text-sm text-slate-600 dark:text-gray-400">
-                        {details.length > 200 ?
-                            <p>{details.slice(0, 120) + '...'} </p> :
-                            <p > {details}</p>
-                        }
+                    <div className="mt-1 text-sm text-slate-600 dark:text-gray-400">
+                        
+                            <p  > {details}</p>
+                        
                     </div>
                 </div>
-
                 <img className="object-cover w-full h-40 mt-2 " src={image_url} alt="---" />
-
                 <div className="flex items-center justify-between px-4 py-1.5 bg-slate-700">
-                    <div className='flex space-x-1'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6.5  text-white">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                        </svg>
-                        <h1 className="text-xl font-bold text-white"
-                        >
-                            {rating.number}
-                        </h1>
-                    </div>
-                    <Link to={`/checkout/${_id}`}  className="text-lg font-medium text-slate-200 transition-colors duration-300 transform"
-                    >Purchase</Link>
+                    <ReactToPdf targetRef={ref} filename="course.pdf">
+                        {({ toPdf }) => (
+
+                            <button onClick={toPdf} className='flex space-x-1'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 text-white">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                </svg>
+
+                            </button>
+
+                        )}
+                    </ReactToPdf>
+                    
+
+                    <Link  to={`/checkout/${_id}`} className="text-lg font-medium text-slate-200 transition-colors duration-300 transform"
+                    >Premium</Link>
 
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
